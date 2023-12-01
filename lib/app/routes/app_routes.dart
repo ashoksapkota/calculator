f4ca59f47aeff186e60f9bd9a7b2f497f7459128
Ashok Sapkota <ashoksapkota720@gmail.com>
2023-12-01 10:41:42 +0545

    first commit

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,13 @@
+
+import 'package:calculator/views/calculator_view.dart';
+
+class AppRoute {
+  AppRoute._();
+  static const String calcRoute = "/";
+
+  static getApplicationRoute() {
+    return {
+      calcRoute: (context) => Calculator(),
+    };
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+
+import 'package:calculator/app/app.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const App());
+}
